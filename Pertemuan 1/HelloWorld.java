445568c7cab8f88c320f236ce74ff98780da876d
MisbahAlvariz <mmisbahalvariz@gmail.com>
2020-10-03 19:55:42 +0700

    Membuat Kode Pertemuan 1

---
+++ Pertemuan 1/HelloWorld.java
@@ -0,0 +1,9 @@
+class HelloWorld{
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+	}
+}
+
+/* 
+program diatas merupakan program sederhana yang akan menghasilkan output = "Hello World!!"
+*/
