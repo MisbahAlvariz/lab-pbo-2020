2ac96cd0e18422c0efbe689b1a3e39bc815424e1
MisbahAlvariz <mmisbahalvariz@gmail.com>
2020-10-04 16:17:54 +0700

    Membuat Kode Pertemuan 1

---
+++ Pertemuan 1/Latihan5.java
@@ -0,0 +1,20 @@
+public class Latihan5
+{
+	public static void main(String args[]) 
+	{ 
+		boolean a = true;
+		boolean b = false;
+
+		System.out.println("a && b = " + (a&&b)); 
+		System.out.println("a || b = " + (a||b) );
+		System.out.println("!(a && b) = " + !(a && b));
+	}	
+}
+
+/*
+	program diatas merupakan program operator logika yang digunakan untuk membandingkan 2 operan yang bertipe boolean
+
+	a && b (bernilai true apabila kedua operan bernilai true, maka disini output yang dihasilkan adalah FALSE karena b bernilai FALSE)
+	a || b (bernilai false apabila kedua operan bernilai false, maka disini output yang dihasilkan adalah TRUE karena keduanya tidak FALSE)
+	!(a && b) (merupakan operator yang membalikan nilai boolean yang artinya jika sebelumnya nilai (a && b) adalah FALSE maka sekarang bernilai TRUE)
+*/
