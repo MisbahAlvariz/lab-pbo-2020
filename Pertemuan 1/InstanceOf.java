4560c313211ac64440f0bdc6759c06f8c58e1d6b
MisbahAlvariz <mmisbahalvariz@gmail.com>
2020-10-04 19:50:40 +0700

    Membuat Kode Pertemuan 1

---
+++ Pertemuan 1/InstanceOf.java
@@ -0,0 +1,15 @@
+public class InstanceOf 
+{
+	public static void main(String args[]) 
+	{
+		String name = "James";
+		
+		boolean result = name instanceof String; 
+		System.out.println( result );
+	}
+}
+
+/*
+	program diatas merupakan program operator Instanceof yang digunakan untuk mengecek apakah suatu objek termasuk ke dalam class tertentu atau tidak. Nilai yang dikembalikan adalah nilai boolean.
+	Output yang dihasilkan pada program diatas adalah TRUE karena name bertipe String
+*/
