0ffe9b9985a50554167bbb4bee7bbd5755332828
MisbahAlvariz <mmisbahalvariz@gmail.com>
2020-10-04 15:39:57 +0700

    Membuat Kode Pertemuan 1

---
+++ Pertemuan 1/Latihan3.java
@@ -0,0 +1,26 @@
+public class Latihan3 
+{	
+	public static void main(String args[]) 
+	{ 
+		int a = 10;
+		int b = 20;
+
+		System.out.println("a == b = " + (a == b) );
+		System.out.println("a != b = " + (a != b) );
+		System.out.println("a > b = " + (a > b) );
+		System.out.println("a < b = " + (a < b) );
+		System.out.println("b >= a = " + (b >= a) );
+		System.out.println("b <= a = " + (b <= a) );
+	}
+}
+
+/*
+	program diatas adalah program operator relasional yang digunakan untuk membandingkan nilai 2 operan
+	
+	a == b (artinya apakah a sama dengan b, maka jawabannya tentu tidak dan menghasilkan output FALSE)
+	a != b (artinya apakah a tidak sama dengan b, maka jawabnya adalah benar dan menghasilkan output TRUE)
+	a > b (artinya apakah a lebih besar dari b, maka jawabnnya adalah salah dan menghasilkan output FALSE)
+	a < b (artinya apakah a lebih kecil dari b, maka jawabnnya adalah benar dan menghasilkan output TRUE)
+	b >= a (artinya apakah b lebih besar dan sama dengan a, maka jawabnnya adalah benar dan menghasilkan output TRUE)
+	b <= a (artinya apakh b lebih kecil dan sama dengan a, maka jawabanya adalah salah dan menghasilkan output FALSE)
+*/
